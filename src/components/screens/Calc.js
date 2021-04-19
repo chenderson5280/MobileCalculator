1303b3c61a942921eb4531bf7be010a6838faecc
Onthecouch <chenderson5280@gmail.com>
2021-04-19 13:45:14 -0600

    color scheme set

--- src/components/screens/Calc.js
+++ src/components/screens/Calc.js
@@ -1,21 +1,64 @@
-import React, {Component} from 'react';
-import {View, Text, TextInput } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, TextInput, StyleSheet, SafeAreaView } from "react-native";
 
 class Calc extends Component {
+    constructor() {
+    super();
+    this.state = {
+        inputText: "",
+    };
+    this.validKeys = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "+",
+        "-",
+        "/",
+        "*",
+        "=",  
+    ];
+}
+
+    handleInput(text) {
+        console.log(text);
+        this.setState({
+        inputText: text,
+        });
+    }
+
     render() {
-        return(
-            <View  style={{flex:1, backgroundColor:'red'}}>
-                <TextInput style={{
-                    height: 150, 
-                    width: 100+"%", 
-                    backgroundColor: 'white'
-                    }} 
-                    />
-            </View>
+        return (
+        <SafeAreaView style={styles.container}>
+            <TextInput
+            onChangeText={this.handleInput.bind(this)}
+            value={this.state.inputText}
+            style={styles.input}
+            />
+        </SafeAreaView>
         );
     }
 }
 
-export default Calc;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgb(161,48,52)",
+    },
+    input: {
+        height: 150,
+        width: "100%",
+        backgroundColor: "rgb(241,235,228)",
+        color: "black",
+        fontSize: 52,
+        textAlign: "right",
+    },
+    });
 
-  
+export default Calc;
